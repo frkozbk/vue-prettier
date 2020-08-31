e467417d6b9ea3aa1619f120c2c5619e7efc1dbe
faruk.ozbek <faruk.ozbek@n11.com>
2020-08-31 15:40:55 +0300

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,33 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint"
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        "plugin:vue/essential",
+        "@vue/eslint-config-airbnb",
+        "eslint:recommended",
+        "@vue/prettier"
+    ],
+    parserOptions: {
+        parser: "babel-eslint"
+    },
+    rules: {
+        "import/no-unresolved": "off",
+        "import/extensions": "off",
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "max-len": [
+            "error",
+            120,
+            4,
+            {
+                ignoreUrls: true,
+                ignoreComments: false,
+                ignoreRegExpLiterals: true,
+                ignoreStrings: false,
+                ignoreTemplateLiterals: false
+            }
+        ]
+    }
 };
